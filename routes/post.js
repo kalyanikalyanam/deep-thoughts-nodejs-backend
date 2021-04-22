2adb337639b4ea41418a2122a4756897c4fbe962
U-DESKTOP-8DA70HV\Possibillion <kalyanamsushma000@gmail.com>
2021-04-22 14:01:51 +0530

    post

--- routes/post.js
+++ routes/post.js
@@ -29,7 +29,33 @@ router.get("/Posts", async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 });
-router.delete("/delete_post", async (req, res) => {
+
+router.get("/update_post/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const postdata = await Post.findById(id);
+
+    res.status(200).json(postdata);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+router.put("/update_post_patch/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatepost = { title, description, _id: id };
+
+  await Post.findByIdAndUpdate(id, updatepost);
+
+  res.json(updatepost);
+});
+
+router.delete("/delete_post/:id", async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id))
